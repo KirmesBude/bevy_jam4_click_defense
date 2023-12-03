d32bbb32cc53f4c29e5d8f300eb09d4e2ffa9476
KirmesBude <kirmesbude@gmail.com>
2023-12-03 17:36:55 +0100

    Update to bevy 0.12.1 and fix wasm build

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // disable console on windows for release builds
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use bevy::winit::WinitWindows;
@@ -11,6 +12,7 @@ use winit::window::Icon;
 
 fn main() {
     App::new()
+        .insert_resource(AssetMetaCheck::Never)
         .insert_resource(Msaa::Off)
         .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
