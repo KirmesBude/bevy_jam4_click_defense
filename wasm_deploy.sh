5dc00f839e9783b4ed05f50d94bdee4848285e28
KirmesBude <kirmesbude@gmail.com>
2023-12-04 23:16:50 +0100

    Fix web deploy (itch)

--- wasm_deploy.sh
+++ wasm_deploy.sh
@@ -2,7 +2,11 @@ cat ./dist/index.html > ./dist/tempindex
 cat ./dist/tempindex | sed -i "s/\"\//\"\.\//g; s/'\//'\.\//g" ./dist/index.html;
 rm ./dist//tempindex
 
+<<comment
 for file in ./dist/*.wasm
 do
     wasm-opt -Os -o "$file" "$file"
 done
+comment
+
+cd dist; zip -r ../bevy_jam4_click_defense.zip *
