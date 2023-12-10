070cf4489423319513e721c935d37f89db8f2ec0
KirmesBude <kirmesbude@gmail.com>
2023-12-10 19:33:18 +0100

    Switch away from kira

--- src/audio.rs
+++ src/audio.rs
@@ -1,17 +1,14 @@
-use std::time::Duration;
-
 use crate::loading::AudioAssets;
 use crate::GameState;
+use bevy::audio::PlaybackMode;
 use bevy::prelude::*;
-use bevy_kira_audio::prelude::*;
 
 pub struct InternalAudioPlugin;
 
 // This plugin is responsible to control the game audio
 impl Plugin for InternalAudioPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(AudioPlugin)
-            .add_systems(OnEnter(GameState::Menu), start_go_audio)
+        app.add_systems(OnEnter(GameState::Menu), start_go_audio)
             .add_systems(
                 OnEnter(GameState::Playing),
                 (stop_go_audio, start_game_audio).chain(),
@@ -19,30 +16,34 @@ impl Plugin for InternalAudioPlugin {
     }
 }
 
-#[derive(Resource)]
-struct GoAudio(Handle<AudioInstance>);
+#[derive(Component)]
+struct GoAudio;
 
-fn start_go_audio(mut commands: Commands, audio_assets: Res<AudioAssets>, audio: Res<Audio>) {
-    audio.pause();
-    let handle = audio
-        .play(audio_assets.go.clone())
-        .looped()
-        .with_volume(1.0)
-        .handle();
-
-    commands.insert_resource(GoAudio(handle));
+fn start_go_audio(mut commands: Commands, audio_assets: Res<AudioAssets>) {
+    commands.spawn((
+        AudioBundle {
+            source: audio_assets.go.clone(),
+            settings: PlaybackSettings {
+                mode: PlaybackMode::Loop,
+                ..Default::default()
+            },
+        },
+        GoAudio,
+    ));
 }
 
-fn stop_go_audio(go_audio: Res<GoAudio>, mut audio_instances: ResMut<Assets<AudioInstance>>) {
-    if let Some(instance) = audio_instances.get_mut(&go_audio.0) {
-        instance.stop(AudioTween::linear(Duration::from_secs_f32(0.5)));
+fn stop_go_audio(mut commands: Commands, go_audio: Query<Entity, With<GoAudio>>) {
+    for entity in &go_audio {
+        commands.entity(entity).despawn_recursive();
     }
 }
 
-fn start_game_audio(audio_assets: Res<AudioAssets>, audio: Res<Audio>) {
-    audio.pause();
-    audio
-        .play(audio_assets.game.clone())
-        .looped()
-        .with_volume(1.0);
+fn start_game_audio(mut commands: Commands, audio_assets: Res<AudioAssets>) {
+    commands.spawn(AudioBundle {
+        source: audio_assets.game.clone(),
+        settings: PlaybackSettings {
+            mode: PlaybackMode::Loop,
+            ..Default::default()
+        },
+    });
 }
--- src/loading.rs
+++ src/loading.rs
@@ -1,7 +1,6 @@
 use crate::GameState;
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
-use bevy_kira_audio::AudioSource;
 
 pub struct LoadingPlugin;
 
